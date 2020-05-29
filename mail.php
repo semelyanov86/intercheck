5180e40bab81a7b8319f27b94a8be1b29eaa4748
Sergey <sergey86@gmail.com>
2020-05-29 13:31:20 +0300

    Created mail script

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?php
+require_once('modules/Emails/class.smtp.php');
+require_once("modules/Emails/class.phpmailer.php");
+
+$mail = new PHPMailer();
+$mail->CharSet = 'UTF-8';
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPDebug = 1;
+
+$mail->Host = 'mail.canada-relocation.com';
+$mail->Port = 25;
+$mail->Username = 'support@canada-relocation.com';
+$mail->Password = 'VZBNniEsuGs93Hwbg5u9aSRzmBE4R9';
+
+$mail->setFrom('support@canada-relocation.com', 'support@canada-relocation.com');
+
+$mail->addAddress('se@sergeyem.ru', 'se@sergeyem.ru');
+
+$mail->Subject = 'This is test message';
+$body = '<p><strong>«This is super test» </strong></p>';
+$mail->msgHTML($body);
+$mail->send();
