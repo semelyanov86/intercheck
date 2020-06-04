3b8263097a28ebdbbd3507e637f68061abb1de73
Sergey <sergey86@gmail.com>
2020-06-04 14:20:24 +0300

    Added field name to comment

--- modules/DocumentApprovals/models/Record.php
+++ modules/DocumentApprovals/models/Record.php
@@ -5,7 +5,7 @@ class DocumentApprovals_Record_Model extends Vtiger_Record_Model
     public static function getNotesForPage($contactid, $page)
     {
         global $adb;
-        $query = "SELECT document_approvalsid, description, page FROM vtiger_document_approvals INNER JOIN vtiger_crmentity ON vtiger_document_approvals.document_approvalsid = vtiger_crmentity.crmid WHERE vtiger_crmentity.deleted = ? AND vtiger_document_approvals.cf_contacts_id = ? AND page = ?";
+        $query = "SELECT document_approvalsid, description, vtiger_document_approvals.name FROM vtiger_document_approvals INNER JOIN vtiger_crmentity ON vtiger_document_approvals.document_approvalsid = vtiger_crmentity.crmid WHERE vtiger_crmentity.deleted = ? AND vtiger_document_approvals.cf_contacts_id = ? AND page = ?";
         $result = $adb->pquery($query, array(0, $contactid, $page));
         $notes = array();
         $notetext = '';
@@ -13,7 +13,7 @@ class DocumentApprovals_Record_Model extends Vtiger_Record_Model
             while ($data = $adb->fetch_array($result)) {
                 if ($data['description']) {
                     $notes[] = $data['description'];
-                    $notetext = $notetext . '||' . $data['description'];
+                    $notetext = $notetext . '||' . $data['name'] . ' - ' . $data['description'];
                 }
             }
         }
